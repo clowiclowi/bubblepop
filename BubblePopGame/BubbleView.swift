7ce271282c49b5cb2df437213ef5b1672c638c60
clowi <chloe.a.wilson@student.uts>
2025-04-25 21:34:38 +1000

    for repo

---
+++ BubblePopGame/BubbleView.swift
@@ -0,0 +1,18 @@
+//
+//  BubbleView.swift
+//  BubblePopGame
+//
+//  Created by Chloe Wilson on 18/4/2025.
+//
+
+import SwiftUI
+
+struct BubbleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BubbleView()
+}
---
+++ BubblePopGame/PlayerNameView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerNameView.swift
+//  BubblePopGame
+//
+//  Created by Chloe Wilson on 18/4/2025.
+//
+
+import SwiftUI
+
+struct PlayerNameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerNameView()
+}
